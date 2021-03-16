95c3da803e881a0d5f19ee065417f2c6dcd984eb
Iris Yu <iris.yu0716@gmail.com>
2021-03-16 13:15:29 +0800

    update

--- instagram.js
+++ instagram.js
@@ -1,6 +1,12 @@
+let fetchCount = 1; // 一次取12筆資料，共取四次，到約39筆的時候跳出登入畫面
+let endCurser = ""; // 下一頁代碼
+let url =
+  "https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=33487574834&first=12&after=" +
+  endCurser;
+
 $.ajax({
-  // url: "https://www.instagram.com/bravo_artofwar/?__a=1",
-  url: "./temp_api.json",
+  url: "https://www.instagram.com/bravo_artofwar/?__a=1",
+  // url: "./temp_api.json",
 }).done(function (result) {
   // 代入profile資訊
   $("#post_count").text(result.graphql.user.edge_owner_to_timeline_media.count);
@@ -18,18 +24,20 @@ $.ajax({
   $("#biography_m").text(result.graphql.user.biography);
 });
 
-$.ajax({
-  // url: "https://www.instagram.com/bravo_artofwar/?__a=1",
-  url:
-    "https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=33487574834&first=12",
-}).done(function (result) {
-  // 顯示圖片
-  const postArray = result.data.user.edge_owner_to_timeline_media.edges;
-  console.log(postArray);
-  for (i = 0; i < postArray.length; i++) {
-    // 貼文是影片
-    if (postArray[i].node.video_view_count) {
-      postContent.innerHTML += `<div class="square co1-4">
+const getPosts = () => {
+  $.ajax({
+    url: "./temp_api.json",
+    // url:
+    //   "https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=33487574834&first=12",
+  }).done(function (result) {
+    // 顯示圖片
+    const postArray = result.data.user.edge_owner_to_timeline_media.edges;
+    console.log(postArray);
+
+    for (i = 0; i < postArray.length; i++) {
+      // 貼文是影片
+      if (postArray[i].node.video_view_count) {
+        postContent.innerHTML += `<div class="square co1-4">
       <img
       class="post_img"
       src="${postArray[i].node.display_url}"
@@ -42,10 +50,10 @@ $.ajax({
         <div class="comment_count">${postArray[i].node.edge_media_to_comment.count}
         </div>
       </div>`;
-    }
-    // 貼文是圖片
-    else {
-      postContent.innerHTML += `<div class="square">
+      }
+      // 貼文是圖片
+      else {
+        postContent.innerHTML += `<div class="square">
         <img
         class="post_img"
         src="${postArray[i].node.display_url}"
@@ -58,54 +66,62 @@ $.ajax({
           <div class="comment_count">${postArray[i].node.edge_media_to_comment.count}
           </div>
         </div>`;
-    }
-  }
-
-  // 顯示圖片右上的小icon
-  const square = document.querySelectorAll(".square");
-  for (i = 0; i < postArray.length; i++) {
-    // 看 api typename是甚麼，決定用甚麼icon
-    let typeName = postArray[i].node.__typename;
-    let dimensions = postArray[i].node.dimensions;
-
-    // let typeName = postArray[i].node.product_type;
-    // if (typeName === "igtv") {
-
-    // 影片類型
-    if (typeName === "GraphVideo") {
-      // 一般影片
-      if (dimensions.height === dimensions.width) {
-        square[
-          i
-        ].innerHTML += `<img class="post_igtv_icon" src="./img/video.png"/>`;
-      } else {
-        // igtv
-        square[
-          i
-        ].innerHTML += `<img class="post_igtv_icon" src="https://img.icons8.com/material-sharp/24/ffffff/igtv.png"/>`;
       }
-      // 多張圖片
-    } else if (typeName === "GraphSidecar") {
-      square[
-        i
-      ].innerHTML += `<img class="post_igtv_icon" src="./img/multipage.png"/>`;
-    } else {
-      // GraphImage
     }
-  }
 
-  // 貼文hover效果
-  for (i = 0; i < postArray.length; i++) {
-    $(".square").on({
-      mouseenter: function () {
-        $(this).children().css("display", "flex");
-      },
-      mouseleave: function () {
-        $(".square_hover").css("display", "none");
-      },
-    });
-  }
-});
+    // 顯示圖片右上的小icon
+    const square = document.querySelectorAll(".square");
+    for (i = 0; i < postArray.length; i++) {
+      // 看 api typename是甚麼，決定用甚麼icon
+      let typeName = postArray[i].node.__typename;
+      let dimensions = postArray[i].node.dimensions;
+
+      // let typeName = postArray[i].node.product_type;
+      // if (typeName === "igtv") {
+
+      // 影片類型
+      if (typeName === "GraphVideo") {
+        // 一般影片
+        if (dimensions.height === dimensions.width) {
+          square[
+            i
+          ].innerHTML += `<img class="post_igtv_icon" src="./img/video.png"/>`;
+        } else {
+          // igtv
+          square[
+            i
+          ].innerHTML += `<img class="post_igtv_icon" src="https://img.icons8.com/material-sharp/24/ffffff/igtv.png"/>`;
+        }
+        // 多張圖片
+      } else if (typeName === "GraphSidecar") {
+        square[
+          i
+        ].innerHTML += `<img class="post_igtv_icon" src="./img/multipage.png"/>`;
+      } else {
+        // GraphImage
+      }
+    }
+
+    // 貼文hover效果
+    for (i = 0; i < postArray.length; i++) {
+      $(".square").on({
+        mouseenter: function () {
+          $(this).children().css("display", "flex");
+        },
+        mouseleave: function () {
+          $(".square_hover").css("display", "none");
+        },
+      });
+    }
+
+    endCurser =
+      result.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
+
+    console.log(endCurser);
+  });
+};
+
+getPosts();
 
 // 為了排版，若一行的貼文數不是三的話補上隱形div
 const postContent = document.querySelector(".post_content");
@@ -120,10 +136,26 @@ if (nodesSameClass.length % 3 === 2) {
   console.log("ok");
 }
 
-// console.log(postArray);
+// window.addEventListener("scroll", function (e) {
+//   if (
+//     (window.innerHeight + window.scrollY) /
+//       document.documentElement.offsetHeight >=
+//     0.999
+//     // Math.ceil(window.innerHeight + window.scrollY) ==
+//     // document.documentElement.offsetHeight
+//   ) {
+//     getPosts();
+//   }
+// });
 
-fetch("https://www.instagram.com/bravo_artofwar/?__a=1")
-  .then((r) => r.json())
-  .then((result) => {
-    console.log("result", result.data.user.edge_followed_by.count);
-  });
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    console.log("near bottom!");
+  }
+});
+
+// fetch("https://www.instagram.com/bravo_artofwar/?__a=1")
+//   .then((r) => r.json())
+//   .then((result) => {
+//     console.log("result", result.data.user.edge_followed_by.count);
+//   });
